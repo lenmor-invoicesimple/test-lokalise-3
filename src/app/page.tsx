6cab23f413c391d573ac8c2150a8e017a7ab4260
Lenmor Dimanalata <lenmor@invoicesimple.com>
2025-09-15 19:18:40 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Page() {
           priority
         />
         <Section />
+        <h2>Test Lokalise 2</h2>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
